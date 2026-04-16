64c37155462a7e1a7763c4381b2319bcb1441a24
pathnirvana <pathnirvana@gmail.com>
2026-04-16 14:03:09 +0530

    update to newest android version

--- app/src/main/java/lk/pitaka/pirith/MainActivity.java
+++ app/src/main/java/lk/pitaka/pirith/MainActivity.java
@@ -1,6 +1,16 @@
 package lk.pitaka.pirith;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+import android.support.v4.media.MediaMetadataCompat;
+import androidx.core.app.NotificationCompat;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.os.Build;
+import android.graphics.BitmapFactory;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +20,8 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaSessionCompat mediaSession;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        WebView myWebView = (WebView) findViewById(R.id.mainWebView);
+        WebView myWebView = findViewById(R.id.mainWebView);
         android.webkit.WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true); // for localStorage
@@ -41,17 +53,19 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setLayoutAlgorithm(android.webkit.WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
         webSettings.setBuiltInZoomControls(true);
         webSettings.setDisplayZoomControls(false);
-        myWebView.addJavascriptInterface(new WebAppInterface(this), "Android"); // not used
+        myWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
         myWebView.setWebViewClient(client);
 
         String webviewLoadUrl = "file:///android_asset/index.html";
         Log.e("LOG_TAG", "webview Url : " + webviewLoadUrl);
         myWebView.loadUrl(webviewLoadUrl);
+
+        setupMediaSession();
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        WebView myWebView = (WebView) findViewById(R.id.mainWebView);
+        WebView myWebView = findViewById(R.id.mainWebView);
         // Check if the key event was the Back button and if there's history
         if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
             myWebView.goBack();
@@ -60,5 +74,80 @@ public class MainActivity extends AppCompatActivity {
         // If it wasn't the Back key or there's no web page history, bubble up to the default
         // system behavior (probably exit the activity)
         return super.onKeyDown(keyCode, event);
+    }
+
+    private void setupMediaSession() {
+        mediaSession = new MediaSessionCompat(this, "PirithMediaSession");
+
+        mediaSession.setCallback(new MediaSessionCompat.Callback() {
+            @Override
+            public void onPlay() {
+                WebView myWebView = findViewById(R.id.mainWebView);
+                if (myWebView != null) myWebView.post(() -> myWebView.evaluateJavascript("if(window.vueMediaControl) window.vueMediaControl('play');", null));
+            }
+
+            @Override
+            public void onPause() {
+                WebView myWebView = findViewById(R.id.mainWebView);
+                if (myWebView != null) myWebView.post(() -> myWebView.evaluateJavascript("if(window.vueMediaControl) window.vueMediaControl('pause');", null));
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaSession != null) {
+            mediaSession.release();
+        }
+    }
+
+    public void updateNativeMediaState(String title, String artist, boolean isPlaying) {
+        if (mediaSession == null) return;
+        
+        int state = isPlaying ? PlaybackStateCompat.STATE_PLAYING : PlaybackStateCompat.STATE_PAUSED;
+        mediaSession.setPlaybackState(new PlaybackStateCompat.Builder()
+                .setActions(PlaybackStateCompat.ACTION_PLAY | PlaybackStateCompat.ACTION_PAUSE)
+                .setState(state, PlaybackStateCompat.PLAYBACK_POSITION_UNKNOWN, 1.0f)
+                .build());
+
+        mediaSession.setMetadata(new MediaMetadataCompat.Builder()
+                .putString(MediaMetadataCompat.METADATA_KEY_TITLE, title)
+                .putString(MediaMetadataCompat.METADATA_KEY_ARTIST, artist)
+                .build());
+
+        mediaSession.setActive(true);
+
+        showMediaNotification(title, artist, isPlaying);
+    }
+
+    private void showMediaNotification(String title, String artist, boolean isPlaying) {
+        String CHANNEL_ID = "media_playback_channel";
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Media Playback", NotificationManager.IMPORTANCE_LOW);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.ic_media_play) 
+                .setContentTitle(title)
+                .setContentText(artist)
+                .setOngoing(isPlaying)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                .setStyle(new androidx.media.app.NotificationCompat.MediaStyle()
+                        .setMediaSession(mediaSession.getSessionToken())
+                        .setShowActionsInCompactView(0));
+
+        if (isPlaying) {
+            builder.addAction(android.R.drawable.ic_media_pause, "Pause", 
+                androidx.media.session.MediaButtonReceiver.buildMediaButtonPendingIntent(this, PlaybackStateCompat.ACTION_PAUSE));
+        } else {
+            builder.addAction(android.R.drawable.ic_media_play, "Play", 
+                androidx.media.session.MediaButtonReceiver.buildMediaButtonPendingIntent(this, PlaybackStateCompat.ACTION_PLAY));
+        }
+
+        notificationManager.notify(1, builder.build());
     }
 }
--- app/src/main/java/lk/pitaka/pirith/WebAppInterface.java
+++ app/src/main/java/lk/pitaka/pirith/WebAppInterface.java
@@ -1,7 +1,9 @@
 package lk.pitaka.pirith;
 
+import android.app.Activity;
 import android.content.Context;
-import android.media.session.MediaSession;
+import android.util.Log;
+import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.widget.Toast;
 
@@ -16,5 +18,51 @@ public class WebAppInterface {
     @JavascriptInterface
     public void showToast(String toast) {
         Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+    }
+    @JavascriptInterface
+    public void updateMediaState(final String title, final String artist, final boolean isPlaying) {
+        if (mContext instanceof MainActivity) {
+            ((Activity) mContext).runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    ((MainActivity) mContext).updateNativeMediaState(title, artist, isPlaying);
+                }
+            });
+        }
+    }
+
+    @JavascriptInterface
+    public void setStatusBarColor(final String colorHex) {
+        if (mContext instanceof Activity) {
+            ((Activity) mContext).runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Activity activity = (Activity) mContext;
+                        int color = android.graphics.Color.parseColor(colorHex);
+                        activity.findViewById(R.id.main).setBackgroundColor(color);
+
+                        double luminance = (0.299 * android.graphics.Color.red(color) +
+                                0.587 * android.graphics.Color.green(color) +
+                                0.114 * android.graphics.Color.blue(color)) / 255;
+
+                        View decorView = activity.getWindow().getDecorView();
+                        int flags = decorView.getSystemUiVisibility();
+
+                        if (luminance > 0.5) {
+                            // Light background -> Dark icons
+                            flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+                        } else {
+                            // Dark background -> Light icons
+                            flags &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+                        }
+                        decorView.setSystemUiVisibility(flags);
+
+                    } catch (Exception e) {
+                        Log.e("LOG_TAG", "Failed to set status bar color: " + e.getMessage());
+                    }
+                }
+            });
+        }
     }
 }
